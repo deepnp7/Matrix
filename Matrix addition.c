46de353c470355ad00c3e36a9c4b7ca32870eed8
Deepnarayan Patra <105645365+deepnp7@users.noreply.github.com>
2023-03-05 13:54:52 +0530

    Update Matrix addition.c

--- Matrix addition.c
+++ Matrix addition.c
@@ -1,1 +1,44 @@
+#include <stdio.h>
+
+int main() {
+   int m, n, i, j;
+   int matrix1[10][10], matrix2[10][10], result[10][10];
+
+   printf("Enter the number of rows and columns of the matrices: ");
+   scanf("%d %d", &m, &n);
+
+   // Input elements of the first matrix
+   printf("\nEnter the elements of the first matrix:\n");
+   for(i = 0; i < m; i++) {
+      for(j = 0; j < n; j++) {
+         scanf("%d", &matrix1[i][j]);
+      }
+   }
+
+   // Input elements of the second matrix
+   printf("\nEnter the elements of the second matrix:\n");
+   for(i = 0; i < m; i++) {
+      for(j = 0; j < n; j++) {
+         scanf("%d", &matrix2[i][j]);
+      }
+   }
+
+   // Add corresponding elements of both matrices
+   for(i = 0; i < m; i++) {
+      for(j = 0; j < n; j++) {
+         result[i][j] = matrix1[i][j] + matrix2[i][j];
+      }
+   }
+
+   // Print the sum matrix
+   printf("\nThe sum of the matrices is:\n");
+   for(i = 0; i < m; i++) {
+      for(j = 0; j < n; j++) {
+         printf("%d ", result[i][j]);
+      }
+      printf("\n");
+   }
+
+   return 0;
+}
 
