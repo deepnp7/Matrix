efc335fa6b8fc93c41ee190274e04e01c2e6e9e1
Deepnarayan Patra <105645365+deepnp7@users.noreply.github.com>
2023-03-21 14:51:10 +0530

    Update Transpose_of_Matrix.c

--- Transpose_of_Matrix.c
+++ Transpose_of_Matrix.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+    printf("Enter the number of rows in the matrix: ");
+    scanf("%d", &rows);
+    printf("Enter the number of columns in the matrix: ");
+    scanf("%d", &cols);
+
+    int matrix[rows][cols];
+    printf("Enter the elements of the matrix: \n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    int transpose[cols][rows];
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            transpose[j][i] = matrix[i][j];
+        }
+    }
+
+    printf("Transpose of the matrix: \n");
+    for (int i = 0; i < cols; i++) {
+        for (int j = 0; j < rows; j++) {
+            printf("%d ", transpose[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
 
