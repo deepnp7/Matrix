3c8834f368d569a35e80766c484ee1c0cd2fb26f
Deepnarayan Patra <105645365+deepnp7@users.noreply.github.com>
2023-03-27 13:51:29 +0530

    Update Transpose_of_Matrix.c

--- Transpose_of_Matrix.c
+++ Transpose_of_Matrix.c
@@ -10,7 +10,7 @@ int main() {
     int matrix[rows][cols];
     printf("Enter the elements of the matrix: \n");
     for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
+        for (int j = 0; j < columns; j++) {
             scanf("%d", &matrix[i][j]);
         }
     }
