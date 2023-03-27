1d12880d5a78e942fb83940c96edb7d7f27f9752
Deepnarayan Patra <105645365+deepnp7@users.noreply.github.com>
2023-03-27 13:52:47 +0530

    Update Transpose_of_Matrix.c

--- Transpose_of_Matrix.c
+++ Transpose_of_Matrix.c
@@ -17,7 +17,7 @@ int main() {
 
     int transpose[columns][rows];
     for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
+        for (int j = 0; j < columns; j++) {
             transpose[j][i] = matrix[i][j];
         }
     }
