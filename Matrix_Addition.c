10f61f18916b62cf4b355c1175a56c1aa3d50f23
Deepnarayan Patra <105645365+deepnp7@users.noreply.github.com>
2023-06-21 11:07:17 +0530

    Update Matrix_Addition.c

--- Matrix_Addition.c
+++ Matrix_Addition.c
@@ -19,7 +19,8 @@ int main() {
 
    // Input elements of the second matrix
    printf("\nEnter the elements of the second matrix:\n");
-   for(i = 0; i < m; i++) {
+   for(i = 0; i < m; i++) 
+   {
       for(j = 0; j < n; j++) {
          scanf("%d", &matrix2[i][j]);
       }
